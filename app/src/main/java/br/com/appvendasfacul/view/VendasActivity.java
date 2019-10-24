1e4ad86cabec77d5906279a03cd6480a716360d3
raphaeldemelo <raphaeldemelosoares@hotmail.com>
2019-10-24 17:10:43 -0300

    deixando código mais limpo

--- app/src/main/java/br/com/appvendasfacul/view/VendasActivity.java
+++ app/src/main/java/br/com/appvendasfacul/view/VendasActivity.java
@@ -1,7 +1,6 @@
 package br.com.appvendasfacul.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,11 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import br.com.appvendasfacul.R;
+import static android.view.View.INVISIBLE;
 
 public class VendasActivity extends AppCompatActivity {
 
@@ -30,21 +28,40 @@ public class VendasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendas);
 
+        BotaoAbrirCupom();
+        BotaoSalvar();
+        ListviewItens();
+
+        campoEan = findViewById(R.id.campoEan);
+        campoQtd = findViewById(R.id.campoQtd);
+        campoQtd.setEnabled(false);
+        campoEan.setEnabled(false);
+    }
+
+    public void BotaoAbrirCupom(){
         final Button btnAbrirCupom = findViewById(R.id.abrirCupomId);
         btnAbrirCupom.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                if (btnAbrirCupom.getText() == "ABRIR CUPOM"){
+
+                if (btnAbrirCupom.getText() == "ABRIR CUPOM") {
+                    lstvwItens.setVisibility(View.VISIBLE);
                     btnAbrirCupom.setText("FECHAR CUPOM");
-                }else{
+                    campoQtd.setEnabled(true);
+                    campoEan.setEnabled(true);
+
+                } else {
+                    lstvwItens.setVisibility(INVISIBLE);
                     btnAbrirCupom.setText("ABRIR CUPOM");
+                    campoQtd.setEnabled(false);
+                    campoEan.setEnabled(false);
                 }
             }
         });
+    }
 
-        campoEan = findViewById(R.id.campoEan);
-        campoQtd  = findViewById(R.id.campoQtd);
-
+    public void BotaoSalvar(){
         btnSalvar = findViewById(R.id.adicionarItemId);
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,23 +69,28 @@ public class VendasActivity extends AppCompatActivity {
                 if (!campoEan.getText().toString().isEmpty() && (!campoQtd.getText().toString().isEmpty())) {
                     itens.add(campoEan.getText().toString());
                     adaptador.notifyDataSetChanged();
-
                     campoEan.setText("");
                     campoQtd.setText("");
-                    Toast.makeText(VendasActivity.this,"adicionado!",Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(VendasActivity.this,"não adicionou! por favor, preencha os campos!",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(VendasActivity.this, "adicionado!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(VendasActivity.this, "não adicionou! por favor, preencha os campos!", Toast.LENGTH_SHORT).show();
+                }
+                if (!campoQtd.isEnabled() && (!campoEan.isEnabled())) {
+                    Toast.makeText(VendasActivity.this, "Abra o cupom para adicionar", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
 
+    public void ListviewItens(){
         lstvwItens = findViewById(R.id.lstvwItem);
-        adaptador = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,itens);
+        lstvwItens.setVisibility(INVISIBLE);
+        adaptador = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, itens);
         lstvwItens.setAdapter(adaptador);
         lstvwItens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(),parent.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
