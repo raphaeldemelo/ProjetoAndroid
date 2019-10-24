c32eb998a26c90742cb4060c29d124ce1387dc43
raphaeldemelo <raphaeldemelosoares@hotmail.com>
2019-10-24 12:33:48 -0300

    adicionando listview

--- app/src/main/java/br/com/appvendasfacul/view/VendasActivity.java
+++ app/src/main/java/br/com/appvendasfacul/view/VendasActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.appvendasfacul.R;
 
 public class VendasActivity extends AppCompatActivity {
+
+    private List<String> itens = new ArrayList<String>();
+    private ArrayAdapter<String> adaptador;
+    private EditText campoEan;
+    private EditText campoQtd;
+    private Button btnSalvar;
+    private ListView lstvwItens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,36 @@ public class VendasActivity extends AppCompatActivity {
                 }else{
                     btnAbrirCupom.setText("ABRIR CUPOM");
                 }
+            }
+        });
+
+        campoEan = findViewById(R.id.campoEan);
+        campoQtd  = findViewById(R.id.campoQtd);
+
+        btnSalvar = findViewById(R.id.adicionarItemId);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!campoEan.getText().toString().isEmpty() && (!campoQtd.getText().toString().isEmpty())) {
+                    itens.add(campoEan.getText().toString());
+                    adaptador.notifyDataSetChanged();
+
+                    campoEan.setText("");
+                    campoQtd.setText("");
+                    Toast.makeText(VendasActivity.this,"adicionado!",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(VendasActivity.this,"não adicionou! por favor, preencha os campos!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        lstvwItens = findViewById(R.id.listContatos);
+        adaptador = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,itens);
+        lstvwItens.setAdapter(adaptador);
+        lstvwItens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),parent.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
             }
         });
     }
